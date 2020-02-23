2c15ad933bddde299745654431f0e69d60b56dd2
Thomas <thomas.hormes@hotmail.de>
2020-02-23 21:24:26 +0100

    Added Spring Boot Actuator; Added Validation Annotations to CarDTO (needed for Validating the Model on Post); If Validation on Post is wrong -> return Bad Request;

--- src/main/java/com/asys1920/dto/CarDTO.java
+++ src/main/java/com/asys1920/dto/CarDTO.java
@@ -2,15 +2,25 @@ package com.asys1920.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Positive;
+
 @Data
 public class CarDTO {
     private Long id;
+    @NotEmpty
     private String name;
+    @NotEmpty
     private String brand;
+    @NotEmpty
     private String model;
+    @Positive
     private int yearOfConstruction;
+    @Positive
     private int numberOfSeats;
+    @Positive
     private int numberOfDoors;
+    @Positive
     private double rentingPricePerDay; //TODO: muss nicht in Car-Objekt gespeichert werden (Business Logik)
     private String vehicleType;
     private boolean isEol;
--- src/main/java/com/asys1920/model/Car.java
+++ src/main/java/com/asys1920/model/Car.java
@@ -9,6 +9,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Positive;
 
 @Entity
 @Data
@@ -19,13 +21,21 @@ public class Car {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+    @NotEmpty
     private String name;
+    @NotEmpty
     private String brand;
+    @NotEmpty
     private String model;
+    @Positive
     private int yearOfConstruction;
+    @Positive
     private int numberOfSeats;
+    @Positive
     private int numberOfDoors;
+    @Positive(message = "WOLOLOLOLO")
     private double rentingPricePerDay; //TODO: muss nicht in Car-Objekt gespeichert werden (Business Logik)
     private VehicleType vehicleType;
     private boolean isEol;
+
 }
